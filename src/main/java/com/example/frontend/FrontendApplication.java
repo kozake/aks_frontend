0cf123f8dccd6cd963530e30da306f363d661f1f
kozake <sh.kozake@gmail.com>
2019-02-24 15:44:24 +0900

    front

---
+++ src/main/java/com/example/frontend/FrontendApplication.java
@@ -0,0 +1,60 @@
+package com.example.frontend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+	public static class Service {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+
+	@Controller()
+	@RequestMapping("/home")
+	public static class HomeController {
+
+		private RestTemplate restTemplate ;
+
+		@Value("${app.backend.url}")
+		private String appBackendUrl;
+
+		public HomeController(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+		}
+
+		@GetMapping()
+		public String init(Model model) {
+
+			Service service = this.restTemplate.getForObject(appBackendUrl + "/service", Service.class);
+			model.addAttribute("name", service.getName());
+			return "home";
+		}
+	}
+
+}
